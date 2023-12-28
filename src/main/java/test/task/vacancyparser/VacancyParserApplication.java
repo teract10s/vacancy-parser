c3d1a43cd79d0afb805367ec1dde9300bd8bc73a
Teract10s <trs.leonov@gmail.com>
2023-12-28 23:40:46 +0200

    init

---
+++ src/main/java/test/task/vacancyparser/VacancyParserApplication.java
@@ -0,0 +1,13 @@
+package test.task.vacancyparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/task/vacancyparser/VacancyParserApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.vacancyparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
