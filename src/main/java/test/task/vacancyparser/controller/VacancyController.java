bfa5a3db2133bb97847428c5a90bbf463c397217
Teract10s <trs.leonov@gmail.com>
2023-12-29 15:14:18 +0200

    added swagger

--- src/main/java/test/task/vacancyparser/controller/VacancyController.java
+++ src/main/java/test/task/vacancyparser/controller/VacancyController.java
@@ -1,5 +1,6 @@
 package test.task.vacancyparser.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import test.task.vacancyparser.dto.JobSearchParameters;
 import test.task.vacancyparser.service.JobService;
 import test.task.vacancyparser.dto.VacancyDto;
+import io.swagger.v3.oas.annotations.Operation;
 
+@Tag(name = "Vacancy management", description = "Endpoints for managing vacancies")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/vacancies")
@@ -17,11 +20,13 @@ public class VacancyController {
     private final JobService jobService;
 
     @GetMapping
+    @Operation(summary = "Get vacancies", description = "Get vacancies by specific job function")
     public List<VacancyDto> getVacancyByFunction(@RequestParam String jobFunction) {
         return jobService.getVacanciesByFunction(jobFunction);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Search vacancies", description = "Find specific jobs in the database by parameters")
     public List<VacancyDto> search(JobSearchParameters searchParameters) {
         return jobService.search(searchParameters);
     }
